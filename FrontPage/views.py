683bb1478005d37512e66274baea9b3ad09e381a
InPizzaWeTrust <roma_li.96@mail.ru>
2020-04-26 21:22:25 +0600

    added new templates and styles

--- FrontPage/views.py
+++ FrontPage/views.py
@@ -1,6 +1,7 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from .models import Post
 from django.utils import timezone
+
 from django.http import HttpResponse
 
 
@@ -8,6 +9,11 @@ def index(request):
     posts = Post.objects.filter(published_date__lte=timezone.now()).order_by('published_date')
     return render(request, 'frontpage/index.html', {'posts': posts})
 
+
+def post_detail(request,pk):
+    Post.objects.get(pk=pk)
+    post = get_object_or_404(Post, pk=pk)
+    return render(request, 'frontpage/post_detail.html', {'post': post})
 
 
 # Create your views here.
